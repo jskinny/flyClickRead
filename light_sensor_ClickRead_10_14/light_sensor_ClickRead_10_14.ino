ef1b50055262118d0116e7605f75338dbf91bd85
jskinny <jamesclotfelter@les-bon-temps.local>
2013-10-14 21:10:45 -0400

    inital

---
+++ light_sensor_ClickRead_10_14/light_sensor_ClickRead_10_14.ino
@@ -0,0 +1,38 @@
+/* Photocell simple testing sketch. 
+ 
+Connect one end of the photocell to 5V, the other end to Analog 0.
+Then connect one end of a 10K resistor from Analog 0 to ground 
+Connect LED from pin 11 through a resistor to ground 
+For more information see http://learn.adafruit.com/photocells */
+ 
+int photocellPin = 0;     // the cell and 10K pulldown are connected to a0
+int photocellReading;     // the analog reading from the sensor divider
+int LEDpin = 9;          // connect Red LED to pin 11 (PWM pin)
+int LEDbrightness;        // 
+void setup(void) {
+  // We'll send debugging information via the Serial monitor
+  Serial.begin(9600);   
+}
+ 
+void loop(void) {
+  photocellReading = analogRead(photocellPin);  
+  char inByte = 0;
+  // if we get a valid byte, read analog ins:
+  if (Serial.available() > 0) {
+    // get incoming byte:
+    inByte = Serial.read();
+  
+  Serial.print(photocellReading); 
+  //Serial.print("Analog reading = ");
+  //Serial.println(photocellReading);     // the raw analog reading
+  
+ 
+  // LED gets brighter the darker it is at the sensor
+  // that means we have to -invert- the reading from 0-1023 back to 1023-0
+ // photocellReading = 1023 - photocellReading;
+  //now we have to map 0-1023 to 0-255 since thats the range analogWrite uses
+  LEDbrightness = map(photocellReading, 200, 700, 0, 255);
+  analogWrite(LEDpin, LEDbrightness);
+ 
+  delay(100);
+}}
